2a77d92865eabfe88dab3ce91ed6591a69bbf398
ROOSENS Simon <simon.roosens@johncockerill.com>
2024-03-04 17:57:38 +0100

    * Implement score & controls

--- fighter.py
+++ fighter.py
@@ -1,28 +1,51 @@
 import pygame
 
 class Fighter():
-    def __init__(self, x, y):
+    def __init__(self, player, x, y):
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
         self.jump = False
-
-    def move(self, screen_width, screen_height):
+        self.health = 100
+        self.hit = False
+        self.alive = True
+        self.attacking = False
+        self.attack_cooldown = 0
+        self.attack_type = 0 # 1: punch ; 2: kick
+        self.controls = {
+            'left': pygame.K_q if player == 1 else pygame.K_k,
+            'right': pygame.K_d if player == 1 else pygame.K_m,
+            'jump': pygame.K_z if player == 1 else pygame.K_o,
+            'attack1': pygame.K_a if player == 1 else pygame.K_i,
+            'attack2': pygame.K_e if player == 1 else pygame.K_p
+        }
+        
+    def move(self, screen_width, screen_height, debug_surf, target, round_over):
         # Defines
         SPEED = 5
         GRAVITY = 2
         # Delta change
         dx = 0
         dy = 0
-        # Keyboard
+
+        # Get key pressed
         key = pygame.key.get_pressed()
-        # Movement
-        if key[pygame.K_q]:                        # Left
+
+        if key[self.controls['left']]:
             dx = -SPEED
-        if key[pygame.K_d]:                        # Right
+        elif key[self.controls['right']]:
             dx = SPEED
-        if key[pygame.K_z] and self.jump == False: # Jump
+        elif key[self.controls['jump']] and not self.jump:
             self.vel_y = -30
             self.jump = True
+        elif key[self.controls['attack1']] or key[self.controls['attack2']]:
+            if key[self.controls['attack1']]:
+                self.attack_type = 1
+            if key[self.controls['attack2']]:
+                self.attack_type = 2
+            self.attack(debug_surf, target)
+
+        if self.attack_cooldown > 0:
+            self.attack_cooldown -= 1
 
         # Apply gravity
         self.vel_y += GRAVITY
@@ -42,5 +65,30 @@ class Fighter():
         self.rect.x += dx
         self.rect.y += dy
 
+    def attack(self, debug_surf, target):
+        if self.attack_cooldown == 0:
+            self.attacking = True
+            if self.attack_type == 1:
+                print("punch")
+                self.attack_cooldown = 10
+                attacking_rect = pygame.Rect(self.rect.centerx, self.rect.y, self.rect.width * 2, self.rect.height / 2)
+            elif self.attack_type == 2:
+                print("kick")
+                self.attack_cooldown = 15
+                attacking_rect = pygame.Rect(self.rect.centerx, self.rect.centery, self.rect.width * 2, self.rect.height / 2)
+            else:
+                print("no attack")
+                attacking_rect = pygame.Rect(self.rect.centerx, self.rect.y, self.rect.width * 2, self.rect.height)
+        
+            if attacking_rect.colliderect(target.rect):
+                target.health -= 10
+                target.hit = True
+            pygame.draw.rect(debug_surf, (0, 255, 0), attacking_rect)
+
     def draw(self, surface):
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
+
+    def update(self):
+        if self.health <= 0:
+            self.health = 0
+            self.alive = False
--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from fighter import Fighter
 # Init pygame
 pygame.init()
 
-# Configure screen size
+# Defines
 SCREEN_WIDTH = 768
 SCREEN_HEIGHT = 494
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+FONT = pygame.font.Font("assets/fonts/VCR_OSD_MONO_1.001.ttf", 30)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -17,6 +20,10 @@ pygame.display.set_caption("Street Fighter Style")
 clock = pygame.time.Clock()
 FPS = 60
 
+# Game variables
+score = [0, 0]
+round_over = False
+
 # Configure background
 bg_image = pygame.image.load("assets/images/background/japan_1.png").convert_alpha()
 
@@ -25,9 +32,20 @@ def draw_bg():
     scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
     screen.blit(scaled_bg, (0,0))
 
+# Function for drawing fighter health bars
+def draw_health_bar(health, x, y):
+  ratio = health / 100
+  pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 204, 34))
+  pygame.draw.rect(screen, BLUE, (x, y, 200 * ratio, 30))
+
+# Function for drawing texts
+def draw_text(text, font, text_col, x, y):
+  img = font.render(text, True, text_col)
+  screen.blit(img, (x, y))
+
 # Create instances of fighter
-fighter_1 = Fighter(100, 280)
-fighter_2 = Fighter(600, 280)
+fighter_1 = Fighter(100, 280, 1)
+fighter_2 = Fighter(600, 280, 2)
 
 # Game loop
 run = True
@@ -35,18 +53,35 @@ while run:
 
     clock.tick(FPS)
 
-    # Draw background
+    # Draw background and HUD
     draw_bg()
+    draw_health_bar(fighter_1.health, 20, 20)
+    draw_health_bar(fighter_2.health, 544, 20)
+    draw_text("P1: " + str(score[0]), FONT, WHITE, 20, 60)
+    draw_text("P2: " + str(score[1]), FONT, WHITE, 580, 60)
 
     # Move fighters
-    fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT)
-    fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT)
+    fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2, round_over)
+    fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_1, round_over)
+
+    # Update fighters logic
+    fighter_1.update()
+    fighter_2.update()
 
     # Draw fighters
     fighter_1.draw(screen)
     fighter_2.draw(screen)
 
-    # Event handler
+    # Manage rounds
+    if round_over == False:
+        if fighter_1.alive == False: # Increment P2 score and end the round
+            score[1] += 1
+            round_over = True
+        if fighter_2.alive == False: # Increment P1 score and end the round
+            score[0] += 1
+            round_over = True
+
+    # Event handlera
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
