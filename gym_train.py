86851af5a4f0b17b94a6fc81109e2b1c36f51e27
ROOSENS Simon <simon.roosens@johncockerill.com>
2024-03-06 15:15:46 +0100

    * Implement environment reset when end of game

--- gym_train.py
+++ gym_train.py
@@ -6,14 +6,14 @@ import pygame
 gym.register(
     id='SFGame-v0',
     entry_point='sfgame_env:SFGameEnv', 
-    kwargs={'test': None} 
+    kwargs={} 
 )
 
 test = [0]
 
 # Test the environment
-env = gym.make('SFGame-v0', test=test)
-obs = env.reset()
+env = gym.make('SFGame-v0')
+obs_t = env.reset()
 env.render()
 
 # Set frame rate
@@ -21,22 +21,31 @@ clock = pygame.time.Clock()
 FPS = 60
 
 done = False
-run = True
-while run:
-    clock.tick(FPS)
-    action = env.action_space.sample()  # Random action selection
-    obs, reward, done, _ = env.step(action)
-    env.render()
-    #print('Reward:', reward)
-    #print('Done:', done)
-    print('Observations:', obs)
 
-    # Event handlera
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+# Run the session X times
+for i in range(1, 10):
+    print(f"Episode: {i}")
+    obs_t = env.reset()
+    done = False
 
-    #pygame.time.wait(10)aa
-    
+    # Train
+    while not done:
+        clock.tick(FPS)
+        action = env.action_space.sample()  # Random action selection
+        obs, reward, done, _ = env.step(action)
+        env.render()
+
+        #print('Reward:', reward)
+        #print('Done:', done)
+        #print('Observations:', obs)
+
+        # Event handlera
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        #pygame.time.wait(10)
+            
+print("Game terminated")
 # Exit game
 pygame.quit()
--- sfgame_env.py
+++ sfgame_env.py
@@ -11,6 +11,7 @@ SCREEN_HEIGHT = 494
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
+ORANGE = (255, 127, 39)
 FONT = 0
 CHARA_ANIMATION_STEPS = [5, 3, 3, 5, 1, 1, 1, 1, 7, 1]
 
@@ -35,7 +36,7 @@ controls_p2 = {
 
 class SFGameEnv(gym.Env):
 
-    def __init__(self, test):
+    def __init__(self):
         super(SFGameEnv, self).__init__()
 
         pygame.init()
@@ -54,13 +55,7 @@ class SFGameEnv(gym.Env):
         #self.clock = pygame.time.Clock()
         #self.FPS = 60
 
-        # Game variables
-        self.score = [0, 0]
-        self.round_over = False
-
-        # Create instances of fighter
-        self.fighter_1 = Fighter(controls_p1, False, 100, 280, self.chara_sheet, CHARA_ANIMATION_STEPS)
-        self.fighter_2 = Fighter(controls_p2, True, 600, 280, self.chara_sheet, CHARA_ANIMATION_STEPS)
+        self.reset()
 
         # Setup available actions
         self.action_space = spaces.Discrete(5)
@@ -87,10 +82,10 @@ class SFGameEnv(gym.Env):
         bar_width = 340 * ratio
         if flip:
             pygame.draw.rect(self.screen, WHITE, (x, y - 2, 344, 34))
-            pygame.draw.rect(self.screen, BLUE, (x + 342 - bar_width, y, bar_width, 30))
+            pygame.draw.rect(self.screen, ORANGE, (x + 342 - bar_width, y, bar_width, 30))
         else:
             pygame.draw.rect(self.screen, WHITE, (x - 2, y - 2, 344, 34))
-            pygame.draw.rect(self.screen, BLUE, (x, y, bar_width, 30))
+            pygame.draw.rect(self.screen, ORANGE, (x, y, bar_width, 30))
 
     # Function for drawing texts
     def draw_text(self, text, font, text_col, x, y):
@@ -99,14 +94,38 @@ class SFGameEnv(gym.Env):
 
     def reset(self):
         print("reset")
+        # Game variables
+        self.score = [0, 0]
+        self.round_over = False
+        # Create instances of fighter
+        self.fighter_1 = Fighter(controls_p1, False, 100, 280, self.chara_sheet, CHARA_ANIMATION_STEPS)
+        self.fighter_2 = Fighter(controls_p2, True, 600, 280, self.chara_sheet, CHARA_ANIMATION_STEPS)
 
     def step(self, action):
         self.fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, self.screen, self.fighter_2, self.round_over)
         self.fighter_2.move_agent(SCREEN_WIDTH, SCREEN_HEIGHT, self.screen, self.fighter_1, self.round_over, action)
-        reward = 1.0
 
+        reward = self.compute_reward()
+        obs = self.compute_obs()
+
+        # Manage rounds
+        if self.round_over == False:
+            if self.fighter_1.alive == False: # Increment P2 score and end the round
+                self.score[1] += 1
+                self.round_over = True
+            if self.fighter_2.alive == False: # Increment P1 score and end the round
+                self.score[0] += 1
+                self.round_over = True
+        
+        return obs, reward, self.round_over, {}
+    
+    def compute_reward(self):
+        reward = 0
+        return reward
+    
+    def compute_obs(self):
         # Update observations
-        observations = {
+        obs = {
             'score_p1': self.score[0],
             'score_p2': self.score[1],
             'pos_x_p1' : self.fighter_1.rect.centerx,
@@ -114,8 +133,8 @@ class SFGameEnv(gym.Env):
             'health_p1': self.fighter_1.health,
             'health_p2': self.fighter_2.health
             }
-        
-        return observations, reward, True, {}
+        return obs
+
 
     def render(self):
         # Draw background and HUD
